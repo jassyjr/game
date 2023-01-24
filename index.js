4dc2c850b8fe7e3193ead584c0b3cad6f2cd3758
jassyjr <116488124+jassyjr@users.noreply.github.com>
2023-01-24 10:40:50 +0300

    Update index.js
    ok

--- index.js
+++ index.js
@@ -9,10 +9,10 @@ document.getElementById("btn2").addEventListener("click",function roll() {
 
 var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
 
-var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
+var randomDiceImage = "images/dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
 
 
-var randomImageSource = "images/" + randomDiceImage; //images/dice1.png - images/dice6.png
+var randomImageSource = "images" + randomDiceImage; //images/dice1.png - images/dice6.png
 
 var image1 = document.querySelectorAll("img")[0];
 
@@ -42,20 +42,16 @@ document.getElementById("btn1").addEventListener("click",function roll() {
 
   var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
   
-  var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
+  var randomDiceImage = "images/dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
   
-  var randomImageSource = "images/" + randomDiceImage; //images/dice1.png - images/dice6.png
+  var randomImageSource = "images" + randomDiceImage; //images/dice1.png - images/dice6.png
   
   var image1 = document.querySelectorAll("img")[0];
   
   image1.setAttribute("src", randomImageSource);
   
   
-  var randomNumber2 = Math.floor(Math.random() * 6) + 1;
-  
-  var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
-  
-  document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+ 
   
   
   //If player 1 wins
